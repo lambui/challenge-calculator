28e860048ddd0dc71e06b89be2d7576acb0bc397
lambui <lbui9@ucmerced.edu>
2019-08-31 00:33:28 -0700

    create basic sum calculator

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace restaurantCalculator
 {
@@ -6,7 +8,27 @@ namespace restaurantCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter string: ");
+            string input = Console.ReadLine();
+
+            List<string> numberStrings = input.Split(",").ToList();
+
+            // parse string into numbers to perform operation
+            List<int> numberList = new List<int>();
+            numberStrings.ForEach(item => {
+                if (int.TryParse(item.Trim(), out int number) == false) { // deny invalid number, including float
+                    return;
+                }
+                if (number <= 0) { // deny negative number
+                    return;
+                }
+                numberList.Add(number);
+            });
+
+            // calculate sum
+            int sum = 0; 
+            numberList.ForEach(item => sum+=item);
+            Console.WriteLine(sum);
         }
     }
 }
